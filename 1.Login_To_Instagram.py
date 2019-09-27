a5a355a3b82e72fb1a10d3cf31fc697d2ce32e8f
sina vakili tazehkandi <sina-vakili@users.noreply.github.com>
2019-09-28 00:36:28 +0330

    Update and rename 1.Login_To_Instgram.py to 1.Login_To_Instagram.py

--- 1.Login_To_Instagram.py
+++ 1.Login_To_Instagram.py
@@ -1,10 +1,12 @@
-#New App Instgram
+#Instgram Bot For Tutotial
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
 class InstgramBot:
+      
+    #Input Username and Password To App
 
     def __init__(self,username , password) :
         self.username = username
@@ -14,7 +16,7 @@ class InstgramBot:
         self.login()
 
 
-
+    #Open Firefox And Login To The Instagram 
     def login(self):
         self.driver.get('https://www.instagram.com/accounts/login/?source=auth_switcher')
         time.sleep(10)
@@ -24,7 +26,7 @@ class InstgramBot:
         self.driver.find_elements_by_xpath("//div[contains(text(),'Log In')]")[0].click()
 
 
-
+    #This Is User And Password Part
 
 if __name__ == '__main__' :
     ig_bot = InstgramBot('My User name' , 'My Password')
